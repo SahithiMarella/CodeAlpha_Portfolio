a684670fcbb58a8b173de410396e8342d98f5ded
Sahithi Marella <sahithiwork2003@gmail.com>
2024-03-11 19:10:45 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function() {
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
